aa7a71d1d7b471e86b78c022ccd400bb0a4ab621
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2025-01-24 21:53:16 +0100

    add the index file and the game object

---
+++ src/game.js
@@ -0,0 +1,10 @@
+import Phaser from "phaser";
+
+export default new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 960,
+    height: 540,
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+});
